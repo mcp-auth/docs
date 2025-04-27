2d146feef232bba6e5e35475e8c582637298814a
Gao Sun <gao@silverhand.io>
2025-04-26 19:47:10 -0700

    refactor: add eslint and tsconfig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,17 @@
-import type {ReactNode} from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import type { ReactNode } from 'react';
 
-export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
+const Home = (): ReactNode => {
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <main>
-        Content goes here.
-      </main>
+      description="Description will go into a meta tag in <head />"
+    >
+      <main>Content goes here.</main>
     </Layout>
   );
-}
+};
+
+export default Home;
