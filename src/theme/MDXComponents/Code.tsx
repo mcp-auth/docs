c037a1b1b30b1e0333a7b68e6f1bf5fbffe5d2ef
Gao Sun <gao@silverhand.io>
2025-05-02 12:34:50 -0700

    refactor: use shiki for syntax highlighting

---
+++ src/theme/MDXComponents/Code.tsx
@@ -0,0 +1,28 @@
+import CodeInline from '@theme/CodeInline';
+import type { Props } from '@theme/MDXComponents/Code';
+import type { ComponentProps, JSX, ReactNode } from 'react';
+import React from 'react';
+
+function shouldBeInline(props: Props) {
+  return (
+    // Empty code blocks have no props.children,
+    // see https://github.com/facebook/docusaurus/pull/9704
+    props.children !== undefined &&
+    React.Children.toArray(props.children).every(
+      (el) => typeof el === 'string' && !el.includes('\n')
+    )
+  );
+}
+
+function CodeBlock(props: ComponentProps<'code'>): JSX.Element {
+  return <code {...props} />;
+}
+
+export default function MDXCode(props: Props): ReactNode {
+  return shouldBeInline(props) ? (
+    <CodeInline {...props} />
+  ) : (
+    // eslint-disable-next-line no-restricted-syntax
+    <CodeBlock {...(props as ComponentProps<typeof CodeBlock>)} />
+  );
+}
