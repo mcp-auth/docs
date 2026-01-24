142b5a865b62a5410a6766252effb3879d60f67a
Xiao Yijun <xiaoyijun@silverhand.io>
2026-01-24 22:04:14 +0800

    docs: add provider guides and simplify tutorial

--- sidebars.ts
+++ sidebars.ts
@@ -28,6 +28,16 @@ const sidebars: SidebarsConfig = {
     },
     {
       type: 'category',
+      label: 'Provider Guides',
+      link: {
+        type: 'generated-index',
+        title: 'Provider Guides',
+        description: 'Detailed configuration guides for different authorization server providers.',
+      },
+      items: ['provider-guides/logto', 'provider-guides/generic'],
+    },
+    {
+      type: 'category',
       label: 'References',
       items: [
         {
